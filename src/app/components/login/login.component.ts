f96826879d4d88252c34fe3d2de08b0d9c9e52fb
Fábio Vicentini <vct.fabio.vct@gmail.com>
2020-01-21 19:56:19 -0300

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public form = {
+    email:null,
+    password:null
 
-  constructor() { }
+  };
+  constructor(private http:HttpClient) { }
+
+  onSubmit(){
+    return this.http.post('http://localhost:8000/api/auth/login', this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+  }
 
   ngOnInit() {
+
   }
 
 }
